d961ff0d0dab4320654c8f68d4d9e9ba81c2bdd5
Maksimyuk <georgiy.maksimyuk@flatstack.dev>
2022-01-30 20:17:23 +0300

    * added minor test for checking getting horoscope

--- tests/horoscopes/sources/horo_mail/test_client.py
+++ tests/horoscopes/sources/horo_mail/test_client.py
@@ -6,5 +6,5 @@ from server.horoscopes.enums import HoroscopeSigns
 class TestClient:
 
     def test_get_horoscope_by_sign(self):
-        horo = HoroMailClient(sign=HoroscopeSigns.PISCES).get_horoscope_by_sign()
+        horo = HoroMailClient(sign=HoroscopeSigns.LEO).get_horoscope_by_sign()
         print(horo)
